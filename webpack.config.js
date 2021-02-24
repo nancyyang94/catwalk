3123980b9676ca2ec30140bd37494ae7ac294cdd
Ochaart <mjhperez@gmail.com>
2021-02-24 09:13:49 -0800

    moved src into client folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './client/src/index.js',
   output: {
     path: path.resolve(__dirname, './public'),
     filename: 'bundle.js',
