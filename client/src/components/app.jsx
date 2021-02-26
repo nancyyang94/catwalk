d8ab97d8f76a5abb704f6a67537732ccab054aca
Ochaart <mjhperez@gmail.com>
2021-02-25 22:14:06 -0800

    installed styled-components

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-console */
 import React from 'react';
+import axios from 'axios';
 import ProductDetail from './ProductDetail';
 import RelatedItemsOutfit from './RelatedItemsOutfit';
 import RatingsReviews from './RatingsReviews';
@@ -7,16 +9,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      product: {},
     };
   }
 
+  componentDidMount() {
+    this.getProduct();
+  }
+
+  getProduct() {
+    axios.get('/products/14931')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          product: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
+    const { product } = this.state;
     return (
       <div>
         Product Page
-        <ProductDetail />
-        <RelatedItemsOutfit />
-        <RatingsReviews />
+        <ProductDetail product_id={product} />
+        <RelatedItemsOutfit product_id={product} />
+        <RatingsReviews product_id={product} />
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const axios = require('axios');
+const config = require('../config');
 
 const app = express();
 const port = 3000;
@@ -9,6 +11,20 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/products/:product_id', (req, res) => {
+  axios({
+    method: 'GET',
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/${req.params.product_id}`,
+    headers: { Authorization: config.TOKEN },
+  })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
