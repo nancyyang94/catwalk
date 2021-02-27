29485419688f566f3682e46e5921ad076f722651
Michael Moriarty <mmoriarty316@gmail.com>
2021-02-26 16:36:33 -0800

    implemented componentDidUpdate for Overview.jsx to access App.jsx data

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+import config from '../../../../config';
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      styles: [{ name: 'no name yet!' }],
     };
   }
 
+  componentDidUpdate(prevProps) {
+    if (prevProps.product.id !== this.props.product.id) {
+      axios({
+        method: 'GET',
+        url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products/${this.props.product.id}/styles`,
+        headers: { Authorization: config.TOKEN },
+      })
+        .then((response) => {
+          this.setState({
+            styles: response.data.results,
+          });
+        })
+        .catch((err) => {
+          throw err;
+        });
+    }
+  }
+
   render() {
-    return (<div>Overview</div>);
+    const { styles } = this.state;
+    const { product } = this.props;
+    const { id } = product;
+    return (
+      <div>
+        <div>
+          {id}
+        </div>
+        <div>
+          {styles[0].name}
+        </div>
+      </div>
+    );
   }
 }
 
