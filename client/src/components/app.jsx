ded2124294763b0d254f61b39ddcd9d212781880
Michael Moriarty <mmoriarty316@gmail.com>
2021-03-02 20:52:31 -0800

    took out dummy data for currentStyle and fixed setState typo

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -10,25 +10,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       product: {},
-      currentStyle: {
-        style_id: 1,
-        name: 'Turquoise',
-        original_price: '140',
-        sale_price: '0',
-        'default?': true,
-        photos: [
-          {
-            thumbnail_url: 'urlplaceholder/style_1_photo_number_thumbnail.jpg',
-            url: 'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=962&amp;q=80',
-          },
-        ],
-        // style name
-        // default price
-        // default is true or false
-        // sale price
-        // style id
-        // photos
-      },
+      currentStyle: {},
     };
     this.getProduct = this.getProduct.bind(this);
     this.updateCurrentStyle = this.updateCurrentStyle.bind(this);
