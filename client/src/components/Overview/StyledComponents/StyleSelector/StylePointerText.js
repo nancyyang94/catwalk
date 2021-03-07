b1ae7df8a1b2b8d30d511ea74f7cc527e3353e76
Michael Moriarty <mmoriarty316@gmail.com>
2021-03-07 15:47:05 -0800

    style selector styling

---
+++ client/src/components/Overview/StyledComponents/StyleSelector/StylePointerText.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StylePointerText = styled.h1`
+  font-size: 16px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-style: bold;
+  letter-spacing: 1.5px;
+`;
+
+export default StylePointerText;
