d77edcdf3ed47ece75a63894d6f641a4ef942994
Michael Moriarty <mmoriarty316@gmail.com>
2021-03-08 12:02:45 -0800

    implemented overlay checkmark on selected style

--- client/src/components/Overview/StyleSelector/StyleSelectorEntry.jsx
+++ client/src/components/Overview/StyleSelector/StyleSelectorEntry.jsx
@@ -2,47 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import StyleImg from '../StyledComponents/StyleSelector/StyleImg';
 import ImgContainer from '../StyledComponents/StyleSelector/ImgContainer';
+import CheckmarkContainer from '../StyledComponents/StyleSelector/CheckmarkContainer';
 
-class StyleSelectorEntry extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: false,
-    };
-  }
+const StyleSelectorEntry = ({
+  style, updateCurrentStyle, styleId, selectedId,
+}) => {
+  const { photos } = style;
+  const photoObj = photos[0];
+  const { thumbnail_url: thumbnail } = photoObj;
 
-  componentDidMount() {
-    const { currentStyle, style } = this.props;
-    const { style_id: currentStyleId } = currentStyle;
-    const { style_id: styleId } = style;
-
-    if (currentStyleId === styleId) {
-      this.setState({
-        selected: true,
-      });
-    }
-  }
-
-  render() {
-    const { style, updateCurrentStyle } = this.props;
-    const { photos } = style;
-    const photoObj = photos[0];
-    const { thumbnail_url: thumbnail } = photoObj;
+  if (styleId === selectedId) {
     return (
       <ImgContainer>
+        <CheckmarkContainer>
+          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
+            <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm7 7.457l-9.005 9.565-4.995-5.865.761-.649 4.271 5.016 8.24-8.752.728.685z" />
+          </svg>
+        </CheckmarkContainer>
         <StyleImg src={`${thumbnail}`} alt="style_thumbnail" onClick={updateCurrentStyle} />
       </ImgContainer>
     );
   }
-}
+
+  return (
+    <ImgContainer>
+      <StyleImg src={`${thumbnail}`} alt="style_thumbnail" onClick={updateCurrentStyle} />
+    </ImgContainer>
+  );
+};
+
 StyleSelectorEntry.propTypes = {
-  currentStyle: PropTypes.shape({ // left skus and default? out for now
-    style_id: PropTypes.number,
-    name: PropTypes.string,
-    original_price: PropTypes.string,
-    sale_price: PropTypes.string,
-    photos: PropTypes.arrayOf(PropTypes.object),
-  }),
+  styleId: PropTypes.number,
+  selectedId: PropTypes.number,
   style: PropTypes.shape({ // left skus and default? out for now
     style_id: PropTypes.number,
     name: PropTypes.string,
@@ -55,7 +46,8 @@ StyleSelectorEntry.propTypes = {
 
 StyleSelectorEntry.defaultProps = {
   style: {},
-  currentStyle: {},
+  styleId: null,
+  selectedId: null,
   updateCurrentStyle: null,
 };
 
---
+++ client/src/components/Overview/StyledComponents/StyleSelector/CheckmarkContainer.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const CheckmarkContainer = styled.div`
+  height: 24px;
+  width: 24px;
+  border-radius: 12px;
+  background-color: white;
+  position: absolute;
+  z-index: 10;
+  top: 7px;
+  right: 7px;
+`;
+
+export default CheckmarkContainer;
