e2a2dad60ae4236a63bce1791ed67941852e4e70
Ochaart <mjhperez@gmail.com>
2021-03-09 08:56:53 -0800

    fixed errors path for metaData and bugs for animation on carousel

--- client/src/components/RatingsReviews/ProductBreakdown/IndividualFactor.jsx
+++ client/src/components/RatingsReviews/ProductBreakdown/IndividualFactor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FactorsContainer from '../styledComponents/FactorsContainer';
-import getReviewsMeta from '/server/getReviewsMeta';
+import getReviewsMeta from '../../../../../server/getReviewsMeta';
 
 function IndividualFactor(props) {
   const { factor } = props;
