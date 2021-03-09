87531e6909ea52c6b9887e198fa1fe7f2597eeb3
Nancy Yang <nyyang.194@gmail.com>
2021-03-09 09:59:38 -0800

    Fixed meta data request.

--- client/src/components/RatingsReviews/ProductBreakdown/IndividualFactor.jsx
+++ client/src/components/RatingsReviews/ProductBreakdown/IndividualFactor.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import FactorsContainer from '../styledComponents/FactorsContainer';
-import getReviewsMeta from '/server/getReviewsMeta';
+
 
 function IndividualFactor(props) {
   const { factor } = props;
