e53df32c79b5d6762f5ac63782b65ab5af2136cb
Nancy Yang <nyyang.194@gmail.com>
2021-03-13 12:51:02 -0800

    Remove animation from ratings reviews.

--- Tests/Client/RatingsReviews/enzyme.test.jsx
+++ Tests/Client/RatingsReviews/enzyme.test.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 // import App from '../../../client/src/components/app';
 import RatingsReviews from '../../../client/src/components/RatingsReviews/RatingsReviews';
+import ReviewsList from '../../../client/src/components/RatingsReviews/ReviewsList/ReviewsList';
 
 describe('RatingsReviews', () => {
   let wrapper;
@@ -14,7 +15,7 @@ describe('RatingsReviews', () => {
   });
 });
 
-describe('RatingsBreakdown', () => {
+describe('BreakdownAndList', () => {
   it('renders the ratings breakdown column', () => {
     const wrapper = shallow(<RatingsReviews />);
     expect(wrapper.find('.ratings')).not.toBeNull();
@@ -24,4 +25,11 @@ describe('RatingsBreakdown', () => {
     const wrapper = shallow(<RatingsReviews />);
     expect(wrapper.find('.reviews')).not.toBeNull();
   });
+
+describe('ButtonClick', () => {
+  it('loads two more reviews upon clicking load more', () => {
+    const wrapper = shallow()
+  })
+})
+
 });
--- client/src/components/RatingsReviews/styledComponents/RatingsReviewsContainer.js
+++ client/src/components/RatingsReviews/styledComponents/RatingsReviewsContainer.js
@@ -11,25 +11,6 @@ const RatingsReviewContainer = styled.div`
   color: #ffffff;
 
 
-.ratings, .reviews {
-  animation-duration: 3.5s;
-  animation-name: slidein;
-}
-
-
-@keyframes slidein {
-  from {
-    margin-top: 100%;
-    height: 300%;
-  }
-
-  to {
-    margin-top: 0%;
-    height: 100%;
-    }
-  }
-
-
   @media only screen and (max-width: 960px) {
     grid-template-columns: 1fr;
     margin-bottom: 5%;
